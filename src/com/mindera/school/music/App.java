89e34e3cbb830ecdeaef9cccf80119e13170b5ba
mariapereira <mariapereira5a20@gmail.com>
2019-12-02 15:36:53 +0000

    commit

--- src/com/mindera/school/music/App.java
+++ src/com/mindera/school/music/App.java
@@ -1,7 +1,31 @@
 package com.mindera.school.music;
 
+import com.mindera.school.music.actions.AddMusicAction;
+import com.mindera.school.music.actions.ListMusicsAction;
+import com.mindera.school.music.ui.Menu;
+import com.mindera.school.music.ui.Option;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Cenas");
+        try {
+            Scanner s = new Scanner(System.in);
+            System.out.println("Digite uma data: ");
+            String dataRecebida = s.nextLine();
+            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            Date dt = df.parse(dataRecebida);
+            System.out.println(dt);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+//        Menu menu = new Menu();
+//        menu.add(new Option("Inserir Musica", new AddMusicAction()));
+//        menu.add(new Option("Listar Musicas", new ListMusicsAction()));
+//        menu.add(new Option("Sair", new NoAction()), true);
+//        menu.render();
     }
 }
---
+++ src/com/mindera/school/music/NoAction.java
@@ -0,0 +1,13 @@
+package com.mindera.school.music;
+
+import com.mindera.school.music.ui.Action;
+
+public class NoAction implements Action {
+    public NoAction() {
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
